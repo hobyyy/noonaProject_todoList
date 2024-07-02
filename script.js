10af00865d416a65895c5567c788511ed8a6ccbb
EunseoKim <dmstj9408@gmail.com>
2024-07-02 22:33:40 +0900

    enter event 처리하는방법 변경(함수중복호출에러), 값을 입력하지 않았을 때 if문 위치 변경

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ let finishButton = document.getElementById("finish-button");
 console.log('userInput : ', userInput)
 
 inputButton.addEventListener("click", addTodo);
-userInput.addEventListener("keyup", function(e) { 
-  if(e.keyCode ===13 && !!userInput.value) {  // 엔터누를때 && 값이 존재할때
-    console.log('value : ', !userInput.value)
-    addTodo()
-    userInput.value = "";
-  } else if(e.keyCode ===13) { alert( '값을 입력해주세요' ); }  // 값이 입력하지 않고 엔터누를 때
+userInput.addEventListener("keypress", function(e) { 
+  if(e.key === "Enter") {  // 엔터누를때
+    // console.log('value : ', !userInput.value)
+    addTodo();
+  }   
 });
 // deleteButton.addEventListener("click", deleteTodo);
 // finishButton.addEventListener("click", finishTodo);
@@ -19,8 +18,17 @@ userInput.addEventListener("focus", function() {userInput.value = "";})
 let todoList = [];
 
 function addTodo() {
+  console.log('값1',userInput.value)
+  if(!userInput.value) {  // 값을 입력하지 않았을 때
+    console.log('값2',userInput.value)
+    console.log('if문',!!userInput.value)
+    alert( '값을 입력해주세요' );
+    return;
+  }
   todoList.push(userInput.value);
   console.log('todoList : ', todoList)
+  userInput.value = "";
+  
   render();
 }
 
@@ -33,8 +41,8 @@ function finishTodo() {
 }
 function render() {
   let resultHTML = '';
+  // console.log('todoList')
   for(let i=0;i<todoList.length;i++) {
-    console.log('for')
     resultHTML += `<div class="task">
       <div>${todoList[i]}</div>
       <div>
