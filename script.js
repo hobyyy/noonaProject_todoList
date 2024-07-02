99578f160865429310906258e1a53d801472cbe8
EunseoKim <dmstj9408@gmail.com>
2024-07-02 19:49:36 +0900

    tab에 밑줄 생성 및 enter event 기능 추가

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ let finishButton = document.getElementById("finish-button");
 console.log('userInput : ', userInput)
 
 inputButton.addEventListener("click", addTodo);
+userInput.addEventListener("keyup", function(e) { 
+  if(e.keyCode ===13 && !!userInput.value) {  // 엔터누를때 && 값이 존재할때
+    console.log('value : ', !userInput.value)
+    addTodo()
+    userInput.value = "";
+  } else if(e.keyCode ===13) { alert( '값을 입력해주세요' ); }  // 값이 입력하지 않고 엔터누를 때
+});
 // deleteButton.addEventListener("click", deleteTodo);
 // finishButton.addEventListener("click", finishTodo);
 
