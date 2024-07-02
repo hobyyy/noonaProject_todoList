bc9a57c56db0e282268ab14b76aff6daa1ccef3b
Eunseo Kim <dmstj9408@gamil.com>
2024-07-02 17:09:08 +0900

    render함수 추가

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ let finishButton = document.getElementById("finish-button");
 console.log('userInput : ', userInput)
 
 inputButton.addEventListener("click", addTodo);
-deleteButton.addEventListener("click", deleteTodo);
-finishButton.addEventListener("click", finishTodo);
+// deleteButton.addEventListener("click", deleteTodo);
+// finishButton.addEventListener("click", finishTodo);
 
 userInput.addEventListener("focus", function() {userInput.value = "";})
 let todoList = [];
 
 function addTodo() {
-  todoList.push(userInput.value)
+  todoList.push(userInput.value);
   console.log('todoList : ', todoList)
+  render();
 }
 
 function deleteTodo() {
@@ -22,4 +23,18 @@ function deleteTodo() {
 
 function finishTodo() {
   
+}
+function render() {
+  let resultHTML = '';
+  for(let i=0;i<todoList.length;i++) {
+    console.log('for')
+    resultHTML += `<div class="task">
+      <div>${todoList[i]}</div>
+      <div>
+        <button id="finish-button">finish</button>
+        <button id="delete-button">delete</button>
+      </div>
+    </div>`
+  }
+  document.getElementById("task-board").innerHTML = resultHTML;
 }
