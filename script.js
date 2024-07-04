22bb7a11b196419f253bb0379e8366dc37b62097
Eunseo Kim <dmstj9408@gamil.com>
2024-07-04 16:10:27 +0900

    🚧 server error뜸(수정중) 코딩장소를 옮겨야해서 어쩔 수 없는 commit

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 let userInput = document.getElementById("user-input");
 let inputButton = document.getElementById("input-button");
-// let deleteButton = document.getElementById("delete-button");
-// let finishButton = document.getElementById("finish-button");
+let tabs = document.querySelectorAll(".tab-area div")
+let date = document.getElementById("date");
+let darkMode = document.getElementById("dark-mode");
+let lightMode = document.getElementById("light-mode");
+date.innerHTML = getToday();
+console.log('darkMode.innerHTML : ', darkMode.outerHTML )
+let darkModeEnabled = false;
 
-console.log('userInput : ', userInput)
+
+darkMode.addEventListener("click", function() {
+  darkModeEnabled = !darkModeEnabled;
+  if (darkModeEnabled) {
+    darkMode.outerHTML = `<i id="dark-mode" class="fa-regular fa-moon"></i>`
+  } else {
+    lightMode.outerHTML = `<i id="light-mode" class="fa-solid fa-sun"></i>`
+  }
+  applyDarkMode(darkModeEnabled);
+});
+
+
+
 
 inputButton.addEventListener("click", addTodo);
+// darkMode.addEventListener("click", changeDarkmode);
 userInput.addEventListener("keypress", function(e) { 
   if(e.key === "Enter") {  // 엔터누를때
     // console.log('value : ', !userInput.value)
     addTodo();
   }   
 });
-// deleteButton.addEventListener("click", deleteTodo);
-// finishButton.addEventListener("click", finishTodo);
+for(let i=0;i<tabs.length;i++) {
+  tabs[i].addEventListener("click", filter);
+}
 
 userInput.addEventListener("focus", function() {userInput.value = "";})
 let todoList = [];
 
 function addTodo() {
-  console.log('값1',userInput.value)
+  // console.log('값1',userInput.value)
   if(!userInput.value) {  // 값을 입력하지 않았을 때
     alert( '값을 입력해주세요' );
     return;
@@ -30,36 +49,86 @@ function addTodo() {
     isComplete : false,
   }
   todoList.push(todo);
-  console.log('todo : ', todo)
+  // console.log('todo : ', todo)
   userInput.value = "";
   
-  render();
+  render(todoList);
 }
 
-function render() {
+function render(list) {
   let resultHTML = '';
-  // console.log('todoList')
-  for(let i=0;i<todoList.length;i++) {
-    if(todoList[i].isComplete == true) {
+  console.log('list1 : ', list)
+  for(let i=0;i<list.length;i++) {
+    if(list[i].isComplete == true) {
       resultHTML += `<div class="task backgroud-gray">
-        <div class="todo-finish">${todoList[i].todoContent}</div>
+        <div class="todo-finish">${list[i].todoContent}</div>
         <div>
-          <i class="fa-regular fa-square-check icon-size" onClick = "finishTodo('${todoList[i].id}')"></i>
-          <i class="fa-solid fa-square-xmark icon-size" onClick = "deleteTodo('${todoList[i].id}')"></i>
+          <i class="fa-regular fa-square-check icon-size" onClick = "finishTodo('${list[i].id}')"></i>
+          <i class="fa-solid fa-square-xmark icon-size" onClick = "deleteTodo('${list[i].id}')"></i>
           </div>
           </div>`
         }else {
           resultHTML += `<div class="task">
-          <div>${todoList[i].todoContent}</div>
+          <div>${list[i].todoContent}</div>
           <div>
-          <i class="fa-regular fa-square icon-size" onClick = "finishTodo('${todoList[i].id}')"></i>
-          <i class="fa-solid fa-square-xmark icon-size" onClick = "deleteTodo('${todoList[i].id}')"></i>
+          <i class="fa-regular fa-square icon-size" onClick = "finishTodo('${list[i].id}')"></i>
+          <i class="fa-solid fa-square-xmark icon-size" onClick = "deleteTodo('${list[i].id}')"></i>
         </div>
       </div>`
     }
 
   }
   document.getElementById("task-board").innerHTML = resultHTML;
+}
+// function render() {
+//   let resultHTML = '';
+//   // console.log('todoList')
+//   for(let i=0;i<todoList.length;i++) {
+//     if(todoList[i].isComplete == true) {
+//       resultHTML += `<div class="task backgroud-gray">
+//         <div class="todo-finish">${todoList[i].todoContent}</div>
+//         <div>
+//           <i class="fa-regular fa-square-check icon-size" onClick = "finishTodo('${todoList[i].id}')"></i>
+//           <i class="fa-solid fa-square-xmark icon-size" onClick = "deleteTodo('${todoList[i].id}')"></i>
+//           </div>
+//           </div>`
+//         }else {
+//           resultHTML += `<div class="task">
+//           <div>${todoList[i].todoContent}</div>
+//           <div>
+//           <i class="fa-regular fa-square icon-size" onClick = "finishTodo('${todoList[i].id}')"></i>
+//           <i class="fa-solid fa-square-xmark icon-size" onClick = "deleteTodo('${todoList[i].id}')"></i>
+//         </div>
+//       </div>`
+//     }
+
+//   }
+//   document.getElementById("task-board").innerHTML = resultHTML;
+// }
+
+function filter(event) {
+  let ongoningList = [];
+  let finishList = [];
+  mode = event.target.id;
+  if(mode === "all"){
+    return render(todoList);
+  }else if(mode === "ongoing"){   // 진행중 
+    console.log('mode : ',mode)
+    for(i=0;i<todoList.length;i++){
+      if(todoList[i].isComplete === false){   
+        ongoningList.push(todoList[i]);
+      }
+    }  
+    return render(ongoningList);
+  }else if(mode === "finish"){    // 완료 
+    console.log('mode : ',mode)
+    for(i=0;i<todoList.length;i++){
+      if(todoList[i].isComplete === true){   
+        finishList.push(todoList[i]);
+      }
+    }
+    return render(finishList);
+  }
 }
 
 function finishTodo(id) {
@@ -71,7 +140,7 @@ function finishTodo(id) {
     }
   }
   console.log('todoList : ',todoList)
-  render()
+  render(todoList);
 }
 function deleteTodo(id) {
   console.log('id : ', id)
@@ -82,9 +151,36 @@ function deleteTodo(id) {
     }
   }
   console.log('todoList : ',todoList)
-  render()
+  render(todoList);
 }
 
 function randomIDGenerate() { // random ID 생성하는 함수
   return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function getToday(){
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = ("0" + (1 + date.getMonth())).slice(-2);
+  let day = ("0" + date.getDate()).slice(-2);
+  let week = date.toString().slice(0, 3);
+
+  return year + "." + month + "." + day + " " + week;
+}
+
+function changeDarkmode() {
+  console.log('hello')
+}
+
+
+
+
+function applyDarkMode(enabled) {
+    if (enabled) {
+        document.body.classList.add("dark-mode"); // ダークモードクラスを追加
+        document.body.classList.remove("light-mode"); // ライトモードクラスを削除
+    } else {
+        document.body.classList.add("light-mode"); // ライトモードクラスを追加
+        document.body.classList.remove("dark-mode"); // ダークモードクラスを削除
+    }
 }
